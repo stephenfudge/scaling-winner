c56fae101473610eb8447fe7406db90df25c2aa8
Stephen Fudge <sfudge@gmail.com>
2023-01-21 14:56:31 -0500

    created dvd and brd pages for tv

---
+++ pages/tv/brd.js
@@ -0,0 +1,37 @@
+import clientPromise from "../../lib/mongodb";
+
+export default function TvBrd({tv}){
+    return(
+        <div>
+            <h1>TV BluRays</h1>
+            <ul>
+                {tv.map((film) => (
+                    <li>
+                        <h2>{film.title}</h2>
+                        <h3>{film.season}</h3>
+                        <h3>{film.format}</h3>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export async function getServerSideProps(){
+    try{
+        const client = await clientPromise;
+        const db = client.db("movies");
+
+        const tv = await db
+            .collection("tv")
+            .find({format: "BRD"})
+            .sort({})
+            .toArray();
+
+        return {
+            props: {tv: JSON.parse(JSON.stringify(tv))}
+        };
+    } catch(e){
+        console.error(e);
+    }
+}
---
+++ pages/tv/dvd.js
@@ -0,0 +1,37 @@
+import clientPromise from "../../lib/mongodb";
+
+export default function TvDvd({tv}){
+    return(
+        <div>
+            <h1>TV DVDs</h1>
+            <ul>
+                {tv.map((film) => (
+                    <li>
+                        <h2>{film.title}</h2>
+                        <h3>{film.season}</h3>
+                        <h3>{film.format}</h3>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export async function getServerSideProps(){
+    try{
+        const client = await clientPromise;
+        const db = client.db("movies");
+
+        const tv = await db
+            .collection("tv")
+            .find({format: "DVD"})
+            .sort({})
+            .toArray();
+
+        return {
+            props: {tv: JSON.parse(JSON.stringify(tv))}
+        };
+    } catch(e){
+        console.error(e);
+    }
+}
