17fb516cb38eaf1dd5495dbaeca69f9e7747781c
Stephen Fudge <sfudge@gmail.com>
2023-01-21 14:49:52 -0500

    created dvd/brd pages for music

---
+++ pages/music/dvd.js
@@ -0,0 +1,38 @@
+import clientPromise from "../../lib/mongodb";
+
+export default function MusicDvd({ music }) {
+  return (
+    <div>
+      <h1>Music DVDs</h1>
+           <ul>
+        {music.map((film) => (
+          <li>
+            <h2>{film.artist}</h2>
+            <h2>{film.title}</h2>
+            <h3>{film.format}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("movies");
+
+    const music = await db
+      .collection("music")
+      .find({ format: 'DVD'})
+      .sort({})
+      // .limit(20)
+      .toArray();
+
+    return {
+      props: { music: JSON.parse(JSON.stringify(music)) },
+    };
+  } catch (e) {
+    console.error(e);
+  }
+}
