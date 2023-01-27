b4d6a945d1f85727e382f8e127fed1b56996e302
Stephen Fudge <sfudge@gmail.com>
2023-01-27 17:47:59 -0500

    moved components folder into pages. added graident css to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <div className="backing">
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </div>
   );
 }
