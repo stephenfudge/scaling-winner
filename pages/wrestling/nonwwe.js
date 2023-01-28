c1aad7dacdfc3c79822b163f6960a29772df7a3e
Stephen Fudge <sfudge@gmail.com>
2023-01-27 20:21:32 -0500

    added promotions into tables

--- pages/wrestling/nonwwe.js
+++ pages/wrestling/nonwwe.js
@@ -1,39 +1,63 @@
 import clientPromise from "../../lib/mongodb";
+import WrestlingHeader from "../components/wrestling";
 
-export default function NonWWE ({ nonwwe }) {
-    return (
-        <div>
-            <h1>Non WWE DVDs and BluRays</h1>
-            <ul>
-                {nonwwe.map((film) => (
-                    <li>
-                        <h2>{film.promotion}</h2>
-                        <h2>{film.title}</h2>
-                        <h2>{film.presentation}</h2>
-                        <h3>{film.format}</h3>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+export default function NonWWE({ nonwwe }) {
+  return (
+    <div>
+      <h1>Non WWE DVDs and BluRays</h1>
+      <WrestlingHeader />
+      <div>
+        <table className="table table-compact w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Promotion</th>
+              <th>Title</th>
+              <th>Presentation Style</th>
+              <th>Media Format</th>
+            </tr>
+          </thead>
+          <tbody>
+            {nonwwe.map((film) => (
+              <tr key={film.id}>
+                <th></th>
+                <th>{film.promotion}</th>
+                <td>{film.title}</td>
+                <td>{film.presentation}</td>
+                <td>{film.format}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Promotion</th>
+              <th>Title</th>
+              <th>Presentation Style</th>
+              <th>Media Format</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
-    try {
-        const client = await clientPromise;
-        const db = client.db("movies");
+  try {
+    const client = await clientPromise;
+    const db = client.db("movies");
 
-        const nonwwe = await db
-            .collection("wrestling")
-            .find({ promotion: {$ne: 'WWE'}})
-            .sort({})
-            // .limit(20)
-            .toArray();
+    const nonwwe = await db
+      .collection("wrestling")
+      .find({ promotion: { $ne: "WWE" } })
+      .sort({ promotion: 1, title: 1})
+      .toArray();
 
-        return {
-            props: { nonwwe: JSON.parse(JSON.stringify(nonwwe)) },
-        };
-    } catch (e) {
-        console.error(e);
-    }
+    return {
+      props: { nonwwe: JSON.parse(JSON.stringify(nonwwe)) },
+    };
+  } catch (e) {
+    console.error(e);
+  }
 }
--- pages/wrestling/wwe.js
+++ pages/wrestling/wwe.js
@@ -1,10 +1,11 @@
 import clientPromise from "../../lib/mongodb";
+import WrestlingHeader from "../components/wrestling";
 
-export default function WWE ({ wwe }){
-    return(
-        <div>
-            <h1>WWE DVDs and BluRays</h1>
-            <ul>
+export default function WWE({ wwe }) {
+  return (
+    <div>
+      <h1>WWE DVDs and BluRays</h1>
+      {/* <ul>
                 {wwe.map((film) => (
                     <li>
                         <h2>{film.promotion}</h2>
@@ -13,27 +14,61 @@ export default function WWE ({ wwe }){
                         <h3>{film.format}</h3>
                     </li>
                 ))}
-            </ul>
-        </div>
-    )
+            </ul> */}
+      <WrestlingHeader />
+      <div>
+        <table className="table table-compact w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Promotion</th>
+              <th>Title</th>
+              <th>Presentation Style</th>
+              <th>Media Format</th>
+            </tr>
+          </thead>
+          <tbody>
+            {wwe.map((film) => (
+              <tr key={film.id}>
+                <th></th>
+                <th>{film.promotion}</th>
+                <td>{film.title}</td>
+                <td>{film.presentation}</td>
+                <td>{film.format}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Promotion</th>
+              <th>Title</th>
+              <th>Presentation Style</th>
+              <th>Media Format</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
-    try {
-        const client = await clientPromise;
-        const db = client.db("movies");
+  try {
+    const client = await clientPromise;
+    const db = client.db("movies");
 
-        const wwe = await db
-            .collection("wrestling")
-            .find({ promotion: 'WWE'})
-            .sort({})
-            // .limit(20)
-            .toArray();
+    const wwe = await db
+      .collection("wrestling")
+      .find({ promotion: "WWE" })
+      .sort({ title: 1})
+      // .limit(20)
+      .toArray();
 
-        return {
-            props: { wwe: JSON.parse(JSON.stringify(wwe)) },
-        };
-    } catch (e) {
-        console.error(e);
-    }
+    return {
+      props: { wwe: JSON.parse(JSON.stringify(wwe)) },
+    };
+  } catch (e) {
+    console.error(e);
+  }
 }
