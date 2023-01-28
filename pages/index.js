8b0e42a50b4827be9b31857da718488f8f7bf011
Stephen Fudge <sfudge@gmail.com>
2023-01-28 18:39:50 -0500

    added bg image for main page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Link from 'next/link'
 
 export default function Home(){
     return(
-        <div className='m-4 p-8 h-screen text-center'>
+        <div className='m-4 p-8 h-screen text-center bgimage'>
           
-            <h1 className='text-center text-4xl underline underline-offset-4 tracking-wide p-2'>Welcome!</h1>
-            <p className='text-l p-9'>This website was created so that I could have an easy way to share my movie collection with friends to plan movie nights in a much easier manner.</p>
+            <h1 className='text-center text-4xl underline underline-offset-4 tracking-wide p-2 text-black'>Welcome!</h1>
+            <p className='text-l p-9 text-black'>This website was created so that I could have an easy way to share my movie collection with friends to plan movie nights in a much easier manner.</p>
             <p>I have broken it up into Feature Films, Music, TV and Professional Wrestling</p>
             
           
