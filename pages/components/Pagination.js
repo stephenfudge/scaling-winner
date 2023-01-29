acf8f1d51a3e80f1d9c7ca621da8e58e64385865
Stephen Fudge <sfudge@gmail.com>
2023-01-28 21:51:29 -0500

    took pagination from tv/pagi and made it into a separate pagination component to reuse

---
+++ pages/components/Pagination.js
@@ -0,0 +1,25 @@
+// import React from "react";
+
+const Pagination = ({ page, totalPages, handlePageChange }) => {
+  return (
+    <div className="pagination">
+      <button
+        className="btn btn-secondary"
+        disabled={page === 1}
+        onClick={() => handlePageChange(page - 1)}
+      >
+        Previous
+      </button>
+      <span>{page} of {totalPages}</span>
+      <button
+        className="btn btn-secondary"
+        disabled={page === totalPages}
+        onClick={() => handlePageChange(page + 1)}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- pages/tv/pagi.js
+++ pages/tv/pagi.js
@@ -1,8 +1,9 @@
 import clientPromise from "../../lib/mongodb";
 import TvHeader from "../components/tv";
 import { useState } from "react";
+import Pagination from "../components/Pagination";
 
-export default function TvPagination({tv}) {
+export default function TvPagination({ tv }) {
   const [page, setPage] = useState(1);
   const limit = 15;
   const totalPages = Math.ceil(tv.length / limit);
@@ -50,49 +51,35 @@ export default function TvPagination({tv}) {
           </tfoot>
         </table>
       </div>
-      <div className="">
-        <button
-          className="btn btn-secondary"
-          disabled={page === 1}
-          onClick={() => handlePageChange(page - 1)}
-        >
-          Previous
-        </button>
-        <span className="">
-          {page} of {totalPages}
-        </span>
-        <button
-          className="btn btn-secondary"
-          disabled={page === totalPages}
-          onClick={() => handlePageChange(page + 1)}
-        >
-          Next
-        </button>
-      </div>
+      <Pagination
+        page={page}
+        totalPages={totalPages}
+        handlePageChange={handlePageChange}
+      />
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-    try {
-      const client = await clientPromise;
-      const db = client.db("movies");
-      const page = context.query.page || 1;
+  try {
+    const client = await clientPromise;
+    const db = client.db("movies");
+    const page = context.query.page || 1;
     //  const limit = 10; // changed limit to 10
     //   const skip = (page - 1) * limit;
-  
-      const tv = await db
-        .collection("tv")
-        .find({})
-        .sort({ title: 1, season: 1 })
-        // .skip(skip)
-        // .limit(limit)
-        .toArray();
-  
-      return {
-        props: { tv: JSON.parse(JSON.stringify(tv)) },
-      };
-    } catch (e) {
-      console.error(e);
-    }
+
+    const tv = await db
+      .collection("tv")
+      .find({})
+      .sort({ title: 1, season: 1 })
+      // .skip(skip)
+      // .limit(limit)
+      .toArray();
+
+    return {
+      props: { tv: JSON.parse(JSON.stringify(tv)) },
+    };
+  } catch (e) {
+    console.error(e);
   }
+}
