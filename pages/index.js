edf54d985fa7903203dd13bdd70df6d30c3e94c5
Stephen Fudge <sfudge@gmail.com>
2023-02-13 22:33:46 -0500

    added gradient to bg image for index and tv brd. added additional text on index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home(){
             <h1 className='text-center text-4xl underline underline-offset-4 tracking-wide p-2 font-bold'>Welcome!</h1>
             <p className='pt-9'>This website was created so that I could have an easy way </p>
          <p>  to share my movie collection with friends to plan movie nights</p>
-            <p className="pt-4">I have broken it into 4 sections</p>
+            <p className="pt-4">I have broken it into 4 sections.  Feature Films, Music, Television Shows, and Professional Wrestling</p>
             
           
         </div>
