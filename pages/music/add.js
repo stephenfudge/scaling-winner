136d822604b102b5973539928c890451114041f7
Stephen Fudge <sfudge@gmail.com>
2023-05-18 12:32:04 -0400

    added you are not logged in to add music/tv/wrestling as well as the logged in header to all the add pages

--- pages/music/add.js
+++ pages/music/add.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useRouter } from "next/router";
+import LoggedInHeader from "../../components/loggedinHeader";
 
-const AddMusic = () => {
+export default function AddMusic() {
+  const { user } = useAuth();
+  const router = useRouter();
   const [artist, setArtist] = useState("");
   const [title, setTitle] = useState("");
   const [format, setFormat] = useState("");
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      setShowMessage(true);
+      const timeout = setTimeout(() => {
+        router.push("/");
+      }, 5000);
+      return () => clearTimeout(timeout);
+    }
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -40,91 +56,97 @@ const AddMusic = () => {
 
   return (
     <div>
-      <h1>Add Music</h1>
-      <form className="w-full max-w-sm" onSubmit={handleSubmit}>
-        {/* Artist */}
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              htmlFor="artist"
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            >
-              Artist
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              type="text"
-              name="artist"
-              value={artist}
-              onChange={(e) => setArtist(e.target.value)}
-            />
-          </div>
-        </div>
+      {!user && showMessage && (
+        <p>You do not have access to this page as you are not logged in</p>
+      )}
+      {user && (
+        <>
+        <LoggedInHeader />
+          <h1>Add Music</h1>
+          <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+            {/* Artist */}
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  htmlFor="artist"
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                >
+                  Artist
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  type="text"
+                  name="artist"
+                  value={artist}
+                  onChange={(e) => setArtist(e.target.value)}
+                />
+              </div>
+            </div>
 
-        {/* Title */}
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              htmlFor="title"
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            >
-              Title
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              type="text"
-              name="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-        </div>
-        {/* Format Dropdown */}
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              htmlFor="format"
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            >
-              Format
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <select
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              name="format"
-              value={format}
-              onChange={(e) => setFormat(e.target.value)}
-            >
-              <option value="" disabled>
-                Select Format
-              </option>
-              <option value="BRD">BRD</option>
-              <option value="DVD">DVD</option>
-            </select>
-          </div>
-        </div>
+            {/* Title */}
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  htmlFor="title"
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                >
+                  Title
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  type="text"
+                  name="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+            </div>
+            {/* Format Dropdown */}
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  htmlFor="format"
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                >
+                  Format
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <select
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  name="format"
+                  value={format}
+                  onChange={(e) => setFormat(e.target.value)}
+                >
+                  <option value="" disabled>
+                    Select Format
+                  </option>
+                  <option value="BRD">BRD</option>
+                  <option value="DVD">DVD</option>
+                </select>
+              </div>
+            </div>
 
-        {/* Submit Button */}
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button
-              type="submit"
-              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            >
-              Add
-            </button>
-          </div>
-        </div>
-      </form>
-      <div>{message && <p>{message}</p>}</div>
+            {/* Submit Button */}
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3"></div>
+              <div className="md:w-2/3">
+                <button
+                  type="submit"
+                  className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                >
+                  Add
+                </button>
+              </div>
+            </div>
+          </form>
+          <div>{message && <p>{message}</p>}</div>
+        </>
+      )}
     </div>
   );
-};
-
-export default AddMusic;
+}
--- pages/tv/add.js
+++ pages/tv/add.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useRouter } from "next/router";
+import LoggedInHeader from "../../components/loggedinHeader";
 
 export default function AddTv() {
+  const { user } = useAuth();
+  const router = useRouter();
   const [title, setTitle] = useState("");
   const [season, setSeason] = useState();
   const [format, setFormat] = useState("");
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      setShowMessage(true);
+      const timeout = setTimeout(() => {
+        router.push("/");
+      }, 5000);
+      return () => clearTimeout(timeout);
+    }
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,7 +43,9 @@ export default function AddTv() {
         setSeason();
         setFormat("");
         setError("");
-        setMessage(`Successfully added season ${season} of "${title}" in the "${format}" format!`)
+        setMessage(
+          `Successfully added season ${season} of "${title}" in the "${format}" format!`
+        );
       } catch (errorMessage) {
         setError(errorMessage);
       }
@@ -38,92 +56,99 @@ export default function AddTv() {
 
   return (
     <div>
-      <h1>Add TV</h1>
-      <form className="w-full max-w-sm" onSubmit={handleSubmit}>
-        {/* Title */}
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              htmlFor="title"
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            >
-              Title
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              type="text"
-              name="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-        </div>
+      {!user && showMessage && (
+        <p>You do not have access to this page as you are not logged in</p>
+      )}
+      {user && (
+        <>
+          <LoggedInHeader />
+          <h1>Add TV</h1>
+          <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+            {/* Title */}
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  htmlFor="title"
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                >
+                  Title
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  type="text"
+                  name="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+            </div>
 
-        {/* Season */}
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              htmlFor="season"
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            >
-              Season
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              type="number"
-              step="1"
-              
-              name="season"
-              value={season}
-              onChange={(e) => parseInt(setSeason(e.target.value))}
-            />
-          </div>
-        </div>
+            {/* Season */}
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  htmlFor="season"
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                >
+                  Season
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  type="number"
+                  step="1"
+                  name="season"
+                  value={season}
+                  onChange={(e) => parseInt(setSeason(e.target.value))}
+                />
+              </div>
+            </div>
 
-        {/* Format Dropdown */}
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              htmlFor="format"
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            >
-              Format
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <select
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              name="format"
-              value={format}
-              onChange={(e) => setFormat(e.target.value)}
-            >
-              <option value="" disabled>
-                Select Format
-              </option>
-              <option value="BRD">BRD</option>
-              <option value="DVD">DVD</option>
-            </select>
-          </div>
-        </div>
+            {/* Format Dropdown */}
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  htmlFor="format"
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                >
+                  Format
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <select
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  name="format"
+                  value={format}
+                  onChange={(e) => setFormat(e.target.value)}
+                >
+                  <option value="" disabled>
+                    Select Format
+                  </option>
+                  <option value="BRD">BRD</option>
+                  <option value="DVD">DVD</option>
+                </select>
+              </div>
+            </div>
 
-        {/* Submit Button */}
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button
-              type="submit"
-              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            >
-              Add
-            </button>
-          </div>
-        </div>
-      </form>
-      <div>{message && <p> {message}</p>}</div>
+            {/* Submit Button */}
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3"></div>
+              <div className="md:w-2/3">
+                <button
+                  type="submit"
+                  className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                >
+                  Add
+                </button>
+              </div>
+            </div>
+          </form>
+          <div>{message && <p> {message}</p>}</div>
+        </>
+      )}
     </div>
   );
 }
