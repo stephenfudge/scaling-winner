eb1f1d87ada8b0f683de32804b3644b07396833a
Stephen Fudge <sfudge@gmail.com>
2023-08-09 12:31:44 -0400

    edited the modal to hide images at smaller screen sizes, limit the overview length and allow modal to close when clicking off it

--- components/modal.js
+++ components/modal.js
@@ -1,28 +1,51 @@
 // modal that opens when clicking on titles. displays poster image, title, overview and a link to imdb provided TMDB API
-export default function MovieModal ({ movieDetails, onClose })  {
-    if (!movieDetails) return null;
-  
-    return (
-      <div className="fixed inset-0 flex items-center justify-center z-10">
-        <div className="bg-white rounded-lg p-4 w-96">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
-            alt={movieDetails.title}
-            className="mb-4"
-          />
-          <h2 className="text-xl font-semibold mb-2">{movieDetails.title}</h2>
-          <p className="mb-4">{movieDetails.overview}</p>
-          <a
-            href={`https://www.imdb.com/title/${movieDetails.imdb_id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline"
-          >
-            View on IMDb
-          </a>
-          <button onClick={onClose} className="ml-4">Close</button>
-        </div>
-      </div>
-    );
+export default function MovieModal({ movieDetails, onClose }) {
+  if (!movieDetails) return null;
+
+  // stops modal from closing unless clicked outside of modal or on close button
+  const stopPropagation = (e) => {
+    e.stopPropagation();
   };
-  
+
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center z-10"
+      onClick={onClose}
+    >
+      <div
+        className="bg-white rounded-lg p-4 md:w-96 w-full max-w-sm mx-auto"
+        onClick={stopPropagation}
+      >
+        {/* movie poster at medium and above screen sizes, hidden on mobile devices */}
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+          alt={movieDetails.title}
+          className="mb-4 md:block hidden"
+        />
+
+        {/* movie title  */}
+        <h2 className="text-xl text-center font-semibold mb-2">
+          {movieDetails.title}
+        </h2>
+
+        {/* summary of movie limited to 200 characters */}
+        <p maxLength={200} className="mb-4 md:text-base text-sm">
+          {movieDetails.overview}
+        </p>
+
+        {/* link to the movie's imdb page */}
+        <a
+          href={`https://www.imdb.com/title/${movieDetails.imdb_id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          View on IMDb
+        </a>
+        <button onClick={onClose} className="ml-4">
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
